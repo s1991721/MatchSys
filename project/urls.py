787aad45d81d1c646bad0f469ccd1d1e28eb2c21
okg008 <okg00000008@gmail.com>
2025-12-09 11:34:01 +0900

    Adjusting the structure

--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from qiuren.views import messages
+from bpmatch.views import messages
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- qiuren/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class QiurenConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "qiuren"

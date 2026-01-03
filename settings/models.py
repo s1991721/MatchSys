29c9c42a13783f6e7206706ca3c21990c59d7abb
jef <jef4267@gmail.com>
2026-01-03 17:09:48 +0900

    创建模型

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class SysSettings(models.Model):
+    name = models.CharField("配置名称（唯一）", max_length=255, unique=True)
+    settings = models.JSONField("配置内容（JSON）")
+    created_by = models.BigIntegerField("创建人ID", null=True, blank=True)
+    created_at = models.DateTimeField("创建时间", auto_now_add=True)
+    updated_by = models.BigIntegerField("更新人ID", null=True, blank=True)
+    updated_at = models.DateTimeField("更新时间", auto_now=True)
+    deleted_at = models.DateTimeField("删除时间（软删）", null=True, blank=True, db_index=True)
+
+    class Meta:
+        db_table = "sys_settings"
+        verbose_name = "系统设置"
+        verbose_name_plural = "系统设置"

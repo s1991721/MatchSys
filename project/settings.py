b12c8fa0f254a0e0acaa9a79eaedb8d996060619
jef <jef4267@gmail.com>
2026-01-09 14:20:11 +0900

    登录状态改为，不活跃后30分钟登出

--- project/settings.py
+++ project/settings.py
@@ -149,6 +149,7 @@ X_FRAME_OPTIONS = "SAMEORIGIN"
 
 # Session expires after 30 minutes of inactivity.
 SESSION_COOKIE_AGE = 30 * 60
+SESSION_SAVE_EVERY_REQUEST = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 

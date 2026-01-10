42f9545661d387d4b73c2a1be3439c2c474a4798
jef <jef4267@gmail.com>
2026-01-10 21:18:02 +0900

    客户管理群发邮件增加类型

--- v1.0_init.sql
+++ v1.0_init.sql
@@ -408,7 +408,7 @@ CREATE TABLE IF NOT EXISTS sent_email_logs
     body        TEXT         NOT NULL COMMENT '邮件正文',
     attachments TEXT         NOT NULL COMMENT '附件列表(JSON字符串)',
 
-    mail_type   INT          NOT NULL COMMENT '邮件类型 0:bp 1:技术者送信 2:案件送信',
+    mail_type   INT          NOT NULL COMMENT '邮件类型 0:bp 1:技术者送信 2:案件送信 -1:其他',
     sent_at     DATETIME     NOT NULL COMMENT '邮件发送时间',
 
     created_by  VARCHAR(100) NOT NULL COMMENT '创建人',
